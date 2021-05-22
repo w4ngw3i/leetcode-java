a375fe55f826cb5580cae58a4a3306d925224e8f
wangwei2 <wangweiobs@foxmail.com>
2021-05-22 14:21:23 +0800

    新增leetcode 32题说明

--- src/com/wangwei/leetcode/string/Solution_32.java
+++ src/com/wangwei/leetcode/string/Solution_32.java
@@ -4,6 +4,27 @@ package com.wangwei.leetcode.string;
  * @Author wangwei
  * @Date 2021/4/2 9:16 上午
  * @Version 1.0
+ *
+ * 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * 示例 1：
+ *
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ *
+ * 示例 2：
+ *
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ *
+ * 示例 3：
+ *
+ * 输入：s = ""
+ * 输出：0
  */
 public class Solution_32 {
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
 }
